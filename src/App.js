c1a4391255649e27ae33ad9c2b3fed7d635f703e
Jayaram <jayvenkat1998@gmail.com>
2022-11-21 20:05:05 -0500

    added store

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router";
 
 import HomeScreen from './property-app/HomeScreen';
+import ProfileScreen from './property-app/ProfileScreen';
+
+
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from "react-redux";
+import userReducer from './property-app/user-reducer';
+
+const store = configureStore(
+    {
+      reducer:
+      {
+        user: userReducer
+      }
+});
+
 
 function App() {
   return (
-    <BrowserRouter>
-    <div className="container">
-      <Routes>
-        <Route path="/tuiter/*"
-          element={<HomeScreen />} />
-      </Routes>
-    </div>
-  </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/"
+            element={<HomeScreen />} />
+
+          <Route path="/account"
+            element={<ProfileScreen />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  </Provider>
   );
 }
+
 
 export default App;
--- src/property-app/HomeScreen/index.js
+++ src/property-app/HomeScreen/index.js
@@ -6,32 +6,36 @@ import properties from './properties.json'
 import NavbarComponent from '../NavbarComponent';
 import SearchComponent from './SearchComponent';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
 
 
 function HomeScreen(){
 
     const [filter, setFilter] = useState('');
+    const test = useSelector((state)=>state.user);
+    console.log(test);
     
 
 
     return(
-        <div className='container-fluid wd-container '>
-            <div>
-                <NavbarComponent />
-            </div>
-            <div className="wd-search-comp">
-                <SearchComponent onSearch={(res)=>{setFilter(res)}}/>
-            </div>
-            <div className="wd-mt-40">
-                <div class="row wd-mb-80 wd-home-gallery">
-                    {   
-                        properties.filter(p => p.name.includes(filter)||filter==='').length===0?<h3>sorry no properties found :(</h3>:
-                        properties.filter(p => p.name.includes(filter)||filter==='').map(property=> <PropertyCard key={property._id} property={property} />)
-                    }
+            <div className='container-fluid wd-container '>
+                <div>
+                    <NavbarComponent />
                 </div>
+                <div className="wd-search-comp">
+                    <SearchComponent onSearch={(res)=>{setFilter(res)}}/>
+                </div>
+                <div className="wd-mt-40">
+                    <div class="row wd-mb-80 wd-home-gallery">
+                        {   
+                            properties.filter(p => p.name.includes(filter)||filter==='').length===0?<h3>sorry no properties found :(</h3>:
+                            properties.filter(p => p.name.includes(filter)||filter==='').map(property=> <PropertyCard key={property._id} property={property} />)
+                        }
+                    </div>
+                </div>
+                <div>FOOTER</div>
             </div>
-            <div>FOOTER</div>
-        </div>
     );
     
 }
