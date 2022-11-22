aa2ed86b5071d360044ba8c3abe04184c46cde25
sumayyanaseem <naseem.s@northeastern.edu>
2022-11-22 13:47:01 -0500

    sign in pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import LoginPage from "./property-app/LoginScreen/LoginPage";
+import SignUpPage from "./property-app/LoginScreen/SignUpPage";
+import {BrowserRouter} from "react-router-dom";
+import {Routes, Route} from "react-router";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<LoginPage/>}/>
+                    <Route path="/signup" element={<SignUpPage/>}/>
+
+                </Routes>
+            </div>
+        </BrowserRouter>
+
     </div>
   );
 }
