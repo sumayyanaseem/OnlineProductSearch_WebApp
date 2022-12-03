e6a98f1145423129a0bcefba53e028156717a538
Akshaya <achennak@Akshayas-MacBook-Air.local>
2022-12-03 01:29:54 -0500

    added few changes to details page

--- src/product-app/ProductScreen/Description/DescriptionComponent2.js
+++ src/product-app/ProductScreen/Description/DescriptionComponent2.js
@@ -52,35 +52,6 @@ const DescriptionComponent2 = ({product}) => {
 
                 </div>
             </div>
-
-            <span className="featurette-divider"></span>
-
-            <div className="row featurette">
-                <div className="col-md-7">
-                    <h2 className="featurette-heading fw-normal lh-1">And lastly, this
-                        one. <span className="text-muted">Checkmate.</span></h2>
-                    <p className="lead">And yes, this is the last block of
-                        representative
-                        placeholder content. Again, not really intended to be actually
-                        read,
-                        simply here to give you a better view of what this would look
-                        like with
-                        some actual content. Your content.</p>
-                </div>
-                <div className="col-md-5">
-                    <svg
-                        className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-                        width="500" height="500" xmlns="http://www.w3.org/2000/svg"
-                        role="img"
-                        aria-label="Placeholder: 500x500"
-                        preserveAspectRatio="xMidYMid slice"
-                        focusable="false"><title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#eee"/>
-                        <text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
-                    </svg>
-
-                </div>
-            </div>
         </div>
     );
 
