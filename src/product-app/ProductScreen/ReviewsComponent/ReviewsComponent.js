ee39ba52016244a85d3b2c5ced9200cb5b6827c8
Jayaram <jayvenkat1998@gmail.com>
2022-12-05 19:15:52 -0500

    product screen reviews component chage

--- src/product-app/ProductScreen/ReviewsComponent/ReviewsComponent.js
+++ src/product-app/ProductScreen/ReviewsComponent/ReviewsComponent.js
@@ -15,6 +15,9 @@ const Review = ({review}) =>
     return (
         <div className="row ms-1 mt-2">
             <div className="row mt-4">
+            <div className="col-1">
+                    <img src={review.image??'/assets/default_dp.jpg'} className="rounded-circle" height={50} width={50} />
+                </div>
                 <p><span className="fw-bolder wd-reviews-font"><b>{review.userFirstName} {review.userLastName}</b></span>
                     <span className="text-secondary wd-lightgrey">  {review.date}</span>
                     {
